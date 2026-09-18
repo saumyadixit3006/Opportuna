0fb4c86147c42ca7a66526555de18ca636e0b773
saumyadixit3006 <saumyadixit3006@gmail.com>
2026-09-18 13:35:07 +0530

    Delete src/main directory

--- src/main/src/main/java/com/opportuna/exception/DatabaseOperationException.java
+++
@@ -1,9 +0,0 @@
-package com.opportuna.exception;
-
-public class DatabaseOperationException extends Exception {
-    private static final long serialVersionUID = 1L;
-
-    public DatabaseOperationException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
